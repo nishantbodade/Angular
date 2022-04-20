f333316fc53f6b26014bea0fe889ab1bf87c7e45
nishant bodade <nishantbodade@gmail.com>
2022-04-20 18:28:58 +0530

    define AppComponent

--- apn-new/src/app/app.component.ts
+++ apn-new/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'pm-root',
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+  <div>My First Component</div>
+  </div>
+  `
+
 })
-export class AppComponent {
-  title = 'apn-new';
+
+export class AppComponent{
+  pageTitle:string='Acme Product managment';
 }
